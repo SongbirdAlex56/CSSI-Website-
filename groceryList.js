3bc12185cd7293296aa3a53a2d39201b928a157d
Mychael Gordon <>
2022-08-04 19:19:48 +0000

    Thursday Afternoon Version

--- groceryList.js
+++ groceryList.js
@@ -31,12 +31,8 @@ window.addEventListener('load', () => {
 		task_edit_el.classList.add('edit');
 		task_edit_el.innerText = 'Edit';
 
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
-
 		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
+		
 
 		task_el.appendChild(task_actions_el);
 
@@ -55,9 +51,6 @@ window.addEventListener('load', () => {
 			}
 		});
 
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
-		});
 	});
 });
   
