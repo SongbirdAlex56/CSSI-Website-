7050ce86c255f16393e1ed81a9c8711003d1f430
Mychael Gordon <>
2022-08-05 16:07:35 +0000

    Current Version

---
+++ script.js
@@ -0,0 +1,8 @@
+const loginButton = document.getByClassName("button");
+
+loginButton.addEventListener("click" , () => {
+  document.location.href = "Login.html";
+}
+
+                             
+                             
